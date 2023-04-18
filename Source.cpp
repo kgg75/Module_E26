2298fd3e4da7eab6ed4ae0b477b82c58cf978a6a
kgg75 <kgg75@mail.ru>
2023-04-18 10:56:15 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -4,11 +4,14 @@
 #include <algorithm>
 #include <math.h>
 #include <time.h>
+#include <chrono>
+
 
 using namespace std;
 
-const int  N = 1000;
-const int  M = 10;
+
+const int  N = 100'000;
+const int  M = 1;	// число потоков
 // = { 5, 3, 2, 4, 8, 9, 6, 1, 1, 7, 9, 3, 8, 2, 6, 3, 5, 5, 4, 7 };
 
 
@@ -32,13 +35,15 @@ int main() {
 
 	vector <thread> th;
 
+	auto begin_time = chrono::high_resolution_clock::now();
+
 	for (auto i = 0; i < N; i += size) {
 		th.push_back(thread(
-			[i, size, &result]
+			[i, size, arr, &result]
 			()
 			{
 				for (auto j = i; j < i + size; j++) {
-					cout << j << '\t' << arr[j] << '\t' << result << endl;
+					//cout << j << '\t' << arr[j] << '\t' << result << endl;
 					result += arr[j];
 				}
 			}
@@ -46,8 +51,12 @@ int main() {
 	}
 
 	for_each(th.begin(), th.end(), [](std::thread& it) { if (it.joinable())	it.join(); } );
+	
+	auto end_time = chrono::high_resolution_clock::now();
+	chrono::duration<double> elapsed = end_time - begin_time;
 
 	cout << result << endl;
+	cout << "Processing time (threads " << M << "): " << elapsed.count() << endl;
 
 	return 0;
 }
