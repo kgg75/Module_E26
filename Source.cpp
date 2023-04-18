adb2ac5f84ff54585116deeed8b2fda5d89b14ee
kgg75 <kgg75@mail.ru>
2023-04-18 10:35:55 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,27 +2,35 @@
 #include <thread>
 #include <vector>
 #include <algorithm>
+#include <math.h>
+#include <time.h>
 
 using namespace std;
 
-const int  N = 20;
-const int  M = 4;
-const int arr[N] = {5, 3, 2, 4, 8, 9, 6, 1, 1, 7, 9, 3, 8, 2, 6, 3, 5, 5, 4, 7};
+const int  N = 1000;
+const int  M = 10;
+// = { 5, 3, 2, 4, 8, 9, 6, 1, 1, 7, 9, 3, 8, 2, 6, 3, 5, 5, 4, 7 };
 
 
-int sum(int start, int& result) {
+/*int sum(int start, int& result) {
 	for (auto i = start; i < start + N / M; i++) {
 		cout << i << '\t' << arr[i] << '\t' << result << endl;
 		result += arr[i];
 	}
 	return result;
-}
+}*/
 
 int main() {
-	vector <thread> th;
+	srand(time(NULL));
+
+	int arr[N];
+	for (auto i = 0; i < N; i++)
+		arr[i] = rand() % 10;
 
 	auto size = N / M;
 	int result = 0;
+
+	vector <thread> th;
 
 	for (auto i = 0; i < N; i += size) {
 		th.push_back(thread(
@@ -30,7 +38,7 @@ int main() {
 			()
 			{
 				for (auto j = i; j < i + size; j++) {
-					cout << i << '\t' << arr[j] << '\t' << result << endl;
+					cout << j << '\t' << arr[j] << '\t' << result << endl;
 					result += arr[j];
 				}
 			}
